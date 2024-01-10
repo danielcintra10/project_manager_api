48f5b563c4078b79b7a27ab01ff68054fa913774
Daniel Cintra <dany.webd3v@gmail.com>
2024-01-10 01:33:45 -0500

    feature: create serializers
    - create ProjectSerializer
    - create TaskSerializer
     * add validation to final date
     * modify to_representation()
    - Modify create and update method to add auditory fields
    - Create tests for both serializers

--- api/serializers.py
+++ api/serializers.py
@@ -2,7 +2,7 @@ from django.contrib.auth.password_validation import validate_password
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-
+from project_manager.models import Project, Task
 
 User = get_user_model()
 
@@ -22,7 +22,7 @@ class UserSerializer(serializers.ModelSerializer):
             "creation_date",
             "last_update",
         )
-        read_only_fields = ("id", "is_active", "creation_date", "last_update", )
+        read_only_fields = ("id", "is_active", "creation_date", "last_update",)
         extra_kwargs = {"password": {"write_only": True}}
 
     def validate_password(self, password):
@@ -58,3 +58,63 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
             'token_type': self.token_type
         }
         return new_data_representation
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    developer = serializers.PrimaryKeyRelatedField(many=False,
+                                                   queryset=User.objects.filter(is_active=True,
+                                                                                deleted=False,
+                                                                                role__exact="D", ))
+    project = serializers.SlugRelatedField(slug_field="code", many=False,
+                                           queryset=Project.objects.filter(is_active=True,
+                                                                           is_deleted=False, ))
+
+    class Meta:
+        model = Task
+        fields = ('code', 'title', 'description', 'project',
+                  'developer', 'is_completed', 'final_date',
+                  'is_active', 'created_at', 'updated_at', )
+        read_only_fields = ('code', 'created_at',
+                            'is_active', 'updated_at', )
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        fields = self.context.get('fields', None)
+        representation.pop('project')
+        if fields:
+            representation['project'] = instance.project.code
+        return representation
+
+    def create(self, validated_data):
+        request = self.context.get("request")
+        validated_data["created_by"] = request.user
+        validated_data["updated_by"] = request.user
+        return super().create(validated_data)
+
+    def update(self, instance, validated_data):
+        request = self.context.get("request")
+        validated_data["updated_by"] = request.user
+        return super().update(instance, validated_data)
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    tasks = TaskSerializer(read_only=True, many=True)
+    project_manager = serializers.PrimaryKeyRelatedField(many=False, queryset=User.objects.filter(is_active=True,
+                                                                                                  deleted=False,
+                                                                                                  role__exact="P", ))
+
+    class Meta:
+        model = Project
+        fields = ('code', 'name', 'description', 'project_manager', 'is_active', 'tasks', 'created_at', 'updated_at', )
+        read_only_fields = ('code', 'is_active', 'created_at', 'updated_at', )
+
+    def create(self, validated_data):
+        request = self.context.get("request")
+        validated_data["created_by"] = request.user
+        validated_data["updated_by"] = request.user
+        return super().create(validated_data)
+
+    def update(self, instance, validated_data):
+        request = self.context.get("request")
+        validated_data["updated_by"] = request.user
+        return super().update(instance, validated_data)

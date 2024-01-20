4aa6b62cb4c037764ea9b95b9315a08de786016f
Daniel Cintra <dany.webd3v@gmail.com>
2024-01-20 13:49:36 -0500

    feature: celery & redis configuration
    - add celery to project settings
    - add celery basic config
    - connect redis to project

---
+++ core/celery.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import, unicode_literals
+import os
+
+from celery import Celery
+from django.conf import settings
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
+app = Celery('core')
+app.conf.enable_utc = False
+
+app.config_from_object(settings, namespace='CELERY')
+
+app.autodiscover_tasks()
--- core/settings.py
+++ core/settings.py
@@ -51,6 +51,8 @@ INSTALLED_APPS = [
     'simple_history',
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
+    'django_celery_results',
+    'django_celery_beat',
 ]
 
 MIDDLEWARE = [
@@ -189,3 +191,16 @@ SIMPLE_JWT = {
     "SLIDING_TOKEN_OBTAIN_SERIALIZER": "rest_framework_simplejwt.serializers.TokenObtainSlidingSerializer",
     "SLIDING_TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSlidingSerializer",
 }
+
+# CELERY SETTINGS
+
+CELERY_BROKER_URL = os.environ.get('CELERY_BROKER_URL')
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_BACKEND = 'django-db'
+CELERY_TIMEZONE = 'America/Havana'
+
+# CELERY BEAT SCHEDULER
+
+CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'

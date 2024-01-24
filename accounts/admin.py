013ff914fa5c43dba0d069315995d8470f1027d3
Daniel Cintra <dany.webd3v@gmail.com>
2024-01-24 11:17:08 -0500

    feature: update user admin
    - update admin save_model() to encrypt users password
    - format updated file using black

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
+from secrets import compare_digest
 
 User = get_user_model()
+
 
 # Register your models here.
 
@@ -20,6 +22,17 @@ class UserAdmin(admin.ModelAdmin):
         "last_name",
         "email",
     ]
+
+    def save_model(self, request, obj, form, change):
+        # Verify if user already exist in the database
+        if obj.pk:
+            user = User.objects.filter(pk=obj.pk).first()
+            if not compare_digest(user.password, obj.password):
+                obj.set_password(obj.password)
+        else:
+            # For new users always encrypt password
+            obj.set_password(obj.password)
+        super().save_model(request, obj, form, change)
 
 
 admin.site.register(User, UserAdmin)
